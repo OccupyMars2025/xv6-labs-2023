9e50b6da281b7a82553a951421d4a01100933929
OccupyMars2025 <none>
2024-02-12 15:19:59 +0800

    learn pointer

---
+++ learn_pointer.c
@@ -0,0 +1,61 @@
+/*
+https://pdos.csail.mit.edu/6.828/2019/lec/pointers.c
+
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+void
+f(void)
+{
+    int a[4];
+    int *b = malloc(16);
+    int *c;
+    int i;
+
+    printf("1: a = %p, b = %p, c = %p\n", a, b, c);
+
+    c = a;
+    for (i = 0; i < 4; i++)
+	    a[i] = 100 + i;
+    c[0] = 200;
+    printf("2: a[0] = %d, a[1] = %d, a[2] = %d, a[3] = %d\n",
+	   a[0], a[1], a[2], a[3]);
+
+    c[1] = 300;
+    *(c + 2) = 301;
+    3[c] = 302;
+    printf("3: a[0] = %d, a[1] = %d, a[2] = %d, a[3] = %d\n",
+	   a[0], a[1], a[2], a[3]);
+
+    c = c + 1;
+    *c = 400;
+    printf("4: a[0] = %d, a[1] = %d, a[2] = %d, a[3] = %d\n",
+	   a[0], a[1], a[2], a[3]);
+    printf("4: a[0] = 0x%x, a[1] = 0x%x, a[2] = 0x%x, a[3] = 0x%x\n",
+	   a[0], a[1], a[2], a[3]);
+    for(int i=0; i<=15; ++i) {
+        printf("0x%x, ", *(((unsigned char*)a) + i));
+        if(((i+1) % 4) == 0){
+            printf("\n");
+        }
+    }
+    
+
+    c = (int *) ((char *) c + 1);
+    *c = 500;
+    printf("5: a[0] = 0x%x, a[1] = 0x%x, a[2] = 0x%x, a[3] = 0x%x\n",
+	   a[0], a[1], a[2], a[3]);
+
+    b = (int *) a + 1;
+    c = (int *) ((char *) a + 1);
+    printf("6: a = %p, b = %p, c = %p\n", a, b, c);
+}
+
+int
+main(int ac, char **av)
+{
+    f();
+    return 0;
+}
